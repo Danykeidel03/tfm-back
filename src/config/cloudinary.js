16c7a5d25b39ce9e11e1408aa3345eb1c50aa7d3
danielfernandez <danielfernandez@craftelier.com>
2025-05-20 13:53:36 +0200

    photos pruebas credenciales

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: dp5ykchgc,
-  api_key: 954231466152522,
-  api_secret: yuoUKat03sBicmof7UK2GP0xQPE,
+  cloud_name: 'dp5ykchgc',
+  api_key: '954231466152522',
+  api_secret: 'yuoUKat03sBicmof7UK2GP0xQPE',
 });
 
 const storage = new CloudinaryStorage({
