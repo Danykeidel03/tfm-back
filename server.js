4e660789ebafd0a081154b41490303d6f4ba7f69
danielfernandez <danielfernandez@craftelier.com>
2025-05-23 10:44:17 +0200

    ws

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const initWebSocket = require('./src/config/websocket');
 (async () => {
     try {
         await connectBD();
-        const server = http.createServer(app)
-        initWebSocket(server)
-        server.listen(3000);
-        app.listen(3000, () => {
-            console.log('Servidor escuchando en el puerto 3000');
-        });
+        const server = http.createServer(app);
+        initWebSocket(server);
+        server.listen(3000, () => console.log('Servidor escuchando en el puerto 3000'));
     } catch (e) {
         console.error('No se ha podido levantar el servidor:', e);
     }
